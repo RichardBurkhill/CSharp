ab412da76e3482d208826bbbddc59516c60e9204
RichardBurkhill <RichardBurkhill4@gmail.com>
2025-07-18 21:12:18 +0100

    Refer to the Service in the Controller

--- EmployeeApi/Controllers/EmployeesController.cs
+++ EmployeeApi/Controllers/EmployeesController.cs
@@ -8,20 +8,25 @@ namespace EmployeeApi.Controllers
     [Route("api/[controller]")]
     public class EmployeesController : ControllerBase
     {
-        private static readonly List<Employee> _employees = new();
+        private readonly EmployeeService _employeeService;
+
+        public EmployeesController(EmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
 
         [HttpGet("{email}")]
         public ActionResult<Employee> Get(string email)
         {
-            var employee = _employees.FirstOrDefault(e => e.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
+            var employee = _employeeService.GetByEmail(email);
             return employee is not null ? Ok(employee) : NotFound();
         }
 
         [HttpPost]
         public ActionResult<Employee> Post(Employee employee)
         {
-            _employees.Add(employee);
-            return CreatedAtAction(nameof(Get), new { email = employee.Email }, employee);
+            var created = _employeeService.Add(employee);
+            return CreatedAtAction(nameof(Get), new { email = created.Email }, created);
         }
     }
 }
